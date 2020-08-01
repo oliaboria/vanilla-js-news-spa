b2a7184383351ccee2e9389c8f50748d5aa4e99a
Olga Boris <olgaboris94@gmail.com>
2020-08-01 13:20:06 +0300

    fetch top stories

---
+++ src/contsants.js
@@ -0,0 +1,8 @@
+export const API_BASE_URL = 'https://hacker-news.firebaseio.com/v0/';
+
+export const API_PATH_NAMES = {
+    item: 'item',
+    topstories: 'topstories.json',
+};
+
+export const PRETTY_PRINT = 'print=pretty';
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -53,6 +53,10 @@ const CONFIG = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env'],
+                        plugins: [
+                            '@babel/plugin-transform-runtime',
+                            '@babel/plugin-transform-regenerator',
+                        ],
                     },
                 },
             },

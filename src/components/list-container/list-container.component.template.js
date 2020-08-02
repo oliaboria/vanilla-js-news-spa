78993f4c789b7b6d2df4eda0daca59fa7bad37d6
Olga Boris <olgaboris94@gmail.com>
2020-08-02 06:47:24 +0300

    styles for comment item

--- src/components/list-container/list-container.component.template.js
+++ src/components/list-container/list-container.component.template.js
@@ -1,6 +1,11 @@
 const template = document.createElement('template');
 
 template.innerHTML = `
+    <style>
+        .list-container {
+            list-style: none;
+        }
+    </style>
     <ol class="list-container"></<ol>
 `;
 
